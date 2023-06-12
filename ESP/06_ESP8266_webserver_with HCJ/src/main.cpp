731a10252f73f00f0c4e4a0b124f25848b06a599
prateekrajgautam <prateekrajgautam@gmail.com>
2023-06-13 01:52:54 +0530

    webserver with LittleFS file upload

--- ESP/06_ESP8266_webserver_with HCJ/src/main.cpp
+++ ESP/06_ESP8266_webserver_with HCJ/src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
-#define LED D0
+#define LED LED_BUILTIN
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <FS.h>
+
+#include <ESP8266mDNS.h>
+
+// #include <FS.h>
 #include <LittleFS.h>  // Include the LittleFS library
 IPAddress    apIP(42, 42, 42, 42);  // Defining a static IP address: local & gateway
                                     // Default IP in AP mode is 192.168.4.1
@@ -21,11 +24,16 @@ bool ledState = false;
 String readFile(const char* path) {
   File file = LittleFS.open(path, "r");
   if (!file) {
+    Serial.printf("\n %s", path);
+    Serial.println("file not found");
     return String();
   }
 
   String content;
+  
+    Serial.println("reading file");
   while (file.available()) {
+    Serial.print(".");
     content += file.readStringUntil('\n');
   }
   file.close();
@@ -33,45 +41,52 @@ String readFile(const char* path) {
   return content;
 }
 
+void mdnsStart(){
+  Serial.println("Starting mDNS...");
+    if (MDNS.begin("esp8266")) {  //Start mDNS with name esp8266
+      Serial.println("mDNS responder started at esp8266.local");
+      Serial.println("MDNS started");
+    }
+  }
 
 void handleIndex() {
 String html = readFile("/index.html");
+  Serial.print("html");
   server.send(200, "text/html", html);}
 
-void handleSCSS() {
-String css = readFile("/calc.scss");
+void handleCSS() {
+  String css = readFile("/style.css");
   server.send(200, "text/css", css);}
 
 void handleJS() {
   String js = readFile("/script.js");
   server.send(200, "application/javascript", js);
 }
-void handleGeneratorJS() {
-  String js = readFile("/generator.js");
-  server.send(200, "application/javascript", js);
-}
+
 
 void handleLED() {
-  String state = server.arg("state");
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  if (state == "on") {
-    digitalWrite(ledPin, HIGH);  // Turn the LED on
-  } else if (state == "off") {
-    digitalWrite(ledPin, LOW);  // Turn the LED off
+String state = server.arg("state");
+  Serial.print("server.arg(\"state\"): ");
+  Serial.println(state);
+  Serial.println("LED state changing...");
+
+  if (state == "on" || state == "ON" || state == "On") {
+    digitalWrite(ledPin, LOW);  // Turn the LED on
+  } else if (state == "off" || state == "OFF" || state == "Off") {
+    digitalWrite(ledPin, HIGH);  // Turn the LED off
   }
   server.send(200, "text/plain", "LED state changed");
 }
 
 
 void setup() {
+  
+	Serial.begin(115200);
   LittleFS.begin();  // Initialize the LittleFS file system
   pinMode ( ledPin, OUTPUT );
   digitalWrite ( ledPin, 0 );
 	
 	delay(1000);
-	Serial.begin(115200);
 	Serial.println();
 	Serial.println("Configuring access point...");
 
@@ -86,11 +101,20 @@ void setup() {
 	Serial.print("AP IP address: ");
 	Serial.println(myIP);
  
+
+
+
+  if(WiFi.status() == WL_CONNECTED) //If WiFi connected to hot spot then start mDNS
+  {
+    MDNS.begin("esp8266");
+    mdnsStart();
+  }
+
+
   server.on("/", handleIndex);
   server.on("/index.html", handleIndex);
-  server.on("/calc.scss", handleSCSS);
+  server.on("/style.css", handleCSS);
   server.on("/script.js", handleJS);
-  server.on("/generator.js", handleGeneratorJS);
 
 
   server.on("/led", handleLED);
@@ -103,4 +127,5 @@ void setup() {
 
 void loop() {
 	server.handleClient();
+  MDNS.update();
 }
